c08f9e8ca8d1a5e7eec557d4bc11df9e837b0d35
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-07-16 10:27:33 -0600

    002 se crearon los primeros 5 botones

--- src/org/manueltavico/view/CalculadoraView.java
+++ src/org/manueltavico/view/CalculadoraView.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.manueltavico.view;
-
-/**
- *
- * @author informatica
- */
+ 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+ 
 public class CalculadoraView {
-    
-}
+    private VBox view;
+    private Label pantalla;
+    private GridPane cuadroBotones;
+
+    //controller
+
+    public CalculadoraView(){
+        view = new VBox (15);
+        view.setPadding(new Insets(15));
+        view.setAlignment(Pos.CENTER);
+        view.setStyle("-fx-background-color: #25D6F5");
+        pantalla = new Label ("0");
+        pantalla.setFont(Font.font("Consolas", FontWeight.BOLD, 40));
+        pantalla.setAlignment(Pos.CENTER_RIGHT);
+        pantalla.setPrefSize(235, 50);
+        cuadroBotones = new GridPane(); 
+        cuadroBotones.setHgap(12);//interlineado entre elementos de la cuadricula 
+        cuadroBotones.setVgap(12);
+        cuadroBotones.setAlignment(Pos.CENTER);//alineado al centro
+
+        //primer boton
+
+
+        Button btnUno = new Button("1");
+        btnUno.setPrefSize(50, 50);
+        btnUno.setStyle("-fx-background-color: #A1B6C3; -fx-text-fill:white; -ft-background-radops:5px");
+
+        Button btnDos = new Button("2");
+        btnDos.setPrefSize(50, 50);
+        
+        Button btnTres = new Button("3");
+        btnTres.setPrefSize(50, 50);
+        
+        Button btnCuatro = new Button("4");
+        btnCuatro.setPrefSize(50, 50);
+        
+        Button btnCinco = new Button("5");
+        btnCinco.setPrefSize(50, 50);
