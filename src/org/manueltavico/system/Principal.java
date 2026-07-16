e21d1e5d78c32f5327c02836440cb6510afbaa29
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-07-16 10:26:23 -0600

    001 se configuraron las propiedades del repositorio

--- src/org/manueltavico/system/Principal.java
+++ src/org/manueltavico/system/Principal.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.manueltavico.system;
+  
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.manueltavico.view.CalculadoraView;
+ public class Principal extends Application{
 
-/**
- *
- * @author informatica
- */
-public class Principal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
+         launch(args);
+
     }
-    
+ 
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //vista de calculadora 
+        CalculadoraView calculadora = new CalculadoraView();
+        //nodos
+        Pane raiz = new Pane(calculadora.getView());
+        //escena(nodoRaiz, ancho, largo)
+        Scene escena = new Scene(raiz, 266,390);
+        //cargar escena y mostrar escenario principal
+        escenarioPrincipal.setTitle("Calculadora de Javier");
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+
+    }
+
 }
+ 
